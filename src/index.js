088d42c99eef8f421c05bee1b53c98ff3ccacce2
Tetiana Karlova <tanyalyt80@gmail.com>
2023-03-30 23:03:10 +0300

    function onLoadMoreClick

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import ApiService from './js/api-service';
 import { galleryMarkup } from './js/markup-template';
-import LoadMoreBtn  from "./js/load-more-btn";
+import LoadMoreBtn from './js/load-more-btn';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -26,68 +26,78 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
 });
 
+let quantityHits = 0;
+
 async function onSearchFormSubmit(e) {
   e.preventDefault();
 
   apiService.query = e.currentTarget.elements.searchQuery.value;
-
-  // const { hits, totalHits } = await apiService.fetchGallery(
-  //   apiService.query,
-  //   apiService.page
-  // );
   loadMoreBtn.show();
- 
-  apiService.resetPage();
+  console.log(apiService.page);
+  apiService.page = 1;
+  const { hits, totalHits } = await apiService.fetchGallery(
+    apiService.query,
+    apiService.page
+  );
 
-  const { hits, totalHits } = await apiService.fetchGallery();
   if (totalHits > 10) {
     loadMoreBtn.enable();
   } else {
     loadMoreBtn.disable();
   }
+
   if (apiService.query.trim() === '') {
     Notify.info(
       `Sorry, there are no images matching your search query. Please try again.`
     );
     return;
   }
-  // quantityGalery();
 
-  // apiService.fetchGallery().then(hits => {
-  //   clearGalleryMarkup();
-  //   appendGalleryMarkup(hits);
-  //   lightbox.refresh();
-  // });
-
-  let quantityHits = 0;
   quantityHits += hits.length;
-  if (hits.length === 0) {
+  if (totalHits === 0) {
     Notify.failure(
       `Sorry, there are no images matching your search query. Please try again.`
     );
     return;
   }
 
-  if (hits.length > 0) {
+  if (totalHits > 0) {
     Notify.info(`Hooray! We found ${totalHits} images.`);
-    apiService.resetPage();
-    clearGalleryMarkup;
+
+    clearGalleryMarkup();
     appendGalleryMarkup(hits);
   }
 
-  if (hits.length === totalHits) {
+  if (quantityHits === totalHits) {
     Notify.info(`We're sorry, but you've reached the end of search results.`);
     loadMoreBtn.enable();
   }
 }
 
 async function onLoadMoreClick() {
-  apiService.incrementPage()
-  loadMoreBtn.show();
-  loadMoreBtn.disable();
-  const { hits, totalHits } = await apiService.fetchGallery();
-  appendGalleryMarkup(hits);
-  loadMoreBtn.enable();
+  try {
+    const { hits, totalHits } = await apiService
+      .fetchGallery
+      // apiService.query,
+      // apiService.page
+      ();
+    apiService.page += 1;
+    console.log(apiService.page);
+    appendGalleryMarkup(hits);
+    loadMoreBtn.show();
+    loadMoreBtn.disable();
+    loadMoreBtn.enable();
+
+    quantityHits = 0;
+    quantityHits += hits.length;
+    if (quantityHits === totalHits) {
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+      loadMoreBtn.enable();
+    }
+  } catch (error) {
+    Notify.failure(error.message, 'Something went wrong!');
+    clearGalleryMarkup();
+  }
 }
 
 function appendGalleryMarkup(hits) {
