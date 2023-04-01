95911858f01eb2d24d0962d83d987bc7efc6bf3a
Yanat58 <tanyalyt80@gmail.com>
2023-04-01 12:16:27 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -15,30 +14,29 @@ const loadMoreBtn = new LoadMoreBtn({
   selector: '.load-more',
   hidden: true,
 });
-console.log(loadMoreBtn)
-const apiService = new ApiService();
 
-refs.searchForm.addEventListener('submit', onSearchFormSubmit);
-loadMoreBtn.refs.button.addEventListener('click', onLoadMoreClick);
+const apiService = new ApiService();
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
 
-let quantityHits = 0;
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMoreClick);
+
 
 async function onSearchFormSubmit(e) {
   e.preventDefault();
 loadMoreBtn.hide();
   apiService.query = e.currentTarget.elements.searchQuery.value;
   
-  console.log(apiService.page);
   apiService.resetPage();
-  console.log(await apiService.fetchGallery())
   const { hits, totalHits } = await apiService.fetchGallery();
 
   if (totalHits > 5) {
+    loadMoreBtn.show()
     loadMoreBtn.enable();
   } else {
     loadMoreBtn.disable();
@@ -51,8 +49,7 @@ loadMoreBtn.hide();
     return;
   }
 
-  quantityHits += hits.length;
-  if (totalHits === 0) {
+    if (totalHits === 0) {
     Notify.failure(
       `Sorry, there are no images matching your search query. Please try again.`
     );
@@ -75,19 +72,15 @@ loadMoreBtn.hide();
 async function onLoadMoreClick() {
   try {
     apiService.incrementPage();
-  console.log('до',apiService.page);
     const { hits, totalHits } = await apiService
       .fetchGallery();
-    console.log('після',apiService.page);
     appendGalleryMarkup(hits);
     
-    // loadMoreBtn.show();
-    // loadMoreBtn.disable();
-    // loadMoreBtn.enable();
+    loadMoreBtn.show();
+    loadMoreBtn.enable();
 
-    quantityHits =0;
-    quantityHits += hits.length;
-    if (quantityHits === totalHits) {
+   console.log(apiService.quantityPage())
+    if (apiService.quantityPage() > totalHits) {
       Notify.info(`We're sorry, but you've reached the end of search results.`);
       loadMoreBtn.hide();
     }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -4,12 +4,13 @@ export default class ApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
+    this.perPage = 5
   }
 
   async fetchGallery() {
     const key = '34679822-1c5a5d4931a74610a4cbe01cd';
     const url = 'https://pixabay.com/api/';
-    const filter = `&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=5`;
+    const filter = `&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}`;
 
     return await axios
       .get(`${url}?key=${key}${filter}`)
@@ -25,8 +26,12 @@ export default class ApiService {
     this.page += 1;
   }
 
-  resetPage() {
-     this.page = 1;
+  resetPage(){
+    this.page = 1;
+  }
+
+  quantityPage() {
+   return this.page *  this.perPage
   }
 
   get query() {
